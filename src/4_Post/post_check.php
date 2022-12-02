0f27e362d88cefd63ba177673dfd5f654c45ecb0
rei1104 <2101202@s.asojuku.ac.jp>
2022-12-02 09:52:48 +0900

    ファイルの追加
    新規登録確認画面
    投稿確認画面

---
+++ src/4_Post/post_check.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>投稿情報確認画面</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+</head>
+<body>
+<div name="maindiv" class="container">
+	</div>
+</div>
+<div style="margin-left:20%;margin-top:5%">
+	<div style="margin-top:5%">以下の内容で不満を投稿しますか？</div>
+</div>
+<div style="margin-left:15%;margin-top:2%">	
+	<div style="margin-top:2%">カテゴリ</div>
+	<div style="margin-top:0%">（入力したカテゴリ名）</div>
+	<div style="margin-top:0%">サブカテゴリ</div>
+	<div style="margin-top:0%">（入力したサブカテゴリ名）</div>
+	<div style="margin-top:0%">不満の内容</div>
+	<div style="margin-top:0%">（入力した不満の内容を表示）</div>
+	<div style="margin-top:0%">改善点</div>
+	<div style="margin-top:0%">（入力した改善点を表示（未入力の場合は「未入力」））</div>
+	<div style="margin-top:0%">不満レベル</div>
+	<div style="margin-top:0%">（入力した不満レベルを表示）</div>
+	<div style="margin-top:0%">企業名</div>
+	<div style="margin-top:0%">（入力した企業名（未入力の場合は「未入力」））</div>
+	<div style="margin-top:0%">店舗・支店名</div>
+	<div style="margin-top:0%">（入力した店舗・支店名（未入力の場合は「未入力」））</div>
+	<div style="margin-top:0%">商品・サービス名</div>
+	<div style="margin-top:0%">（入力した商品・サービス名（未入力の場合は「未入力」））</div>
+</div>
+<div style="display:flex;margin-top:3%;">
+    <div style="margin-left:15%;">
+    <input type='submit'value="修正する" style="background-color:" onmouseover="this.style.background=''" onmouseout="this.style.background=''">
+    </div>
+    <div style="margin-left:3%;">
+    <input type='submit'value="登録する" style="background-color:" onmouseover="this.style.background=''" onmouseout="this.style.background=''">
+    </div>
+
+
+</div>
+</body>
+</html>
